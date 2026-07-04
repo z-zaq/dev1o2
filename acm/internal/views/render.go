67819f1207f7222e65847963bb0ef3e8df346197
abdyahaya <yahayakhalid21@gmail.com>
2026-07-04 05:30:31 +0100

    renderedtmpl

---
+++ acm/internal/views/render.go
@@ -0,0 +1,18 @@
+package views
+
+import (
+	"html/template"
+	"net/http"
+)
+func RenderTemplate(w http.ResponseWriter, file string){
+	tmpl, err := template.ParseFiles(
+		"templates/base.html", "templates/"+file)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
