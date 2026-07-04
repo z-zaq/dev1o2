874d7f9ca45c18b16364304f65831a688d8d5458
abdyahaya <yahayakhalid21@gmail.com>
2026-07-04 05:39:36 +0100

    addedhandlerspath

--- acm/internal/handlers/home.go
+++ acm/internal/handlers/home.go
@@ -1,1 +1,15 @@
-package main
+package handlers
+
+import (
+	"acm/internal/views"
+	"net/http"
+)
+func HomeHandler(w http.ResponseWriter, r*http.Request){
+	views.RenderTemplate(w, "home.html")
+}
+func AboutHandler(w http.ResponseWriter, r*http.Request){
+	views.RenderTemplate(w, "about.html")
+}
+func ContactHandler(w http.ResponseWriter, r*http.Request){
+	views.RenderTemplate(w, "contact.html")
+}
